7a06fb03e0ad92fc0b5ba07a340949e5b957a02b
Kacper Falecki <kfalecki17@gmail.com>
2023-02-11 13:43:01 +0100

    Prepare App.tsx to use routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import './App.css';
 
+const router = createBrowserRouter([
+  {}
+])
+
 function App() {
-  return 
+  return <RouterProvider router={router} />
 }
 
 export default App;

ca26ec6cb30fcd8432ef06d2b1588fba98c6aff5
Kacper Falecki <kfalecki17@gmail.com>
2023-02-11 13:28:03 +0100

    Add Copyright component

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,20 @@
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+export function Copyright(props: any) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://github.com/LilWebDeveloper">
+          Website by Takfornying x Falecki
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }

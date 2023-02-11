6a2addce1c1b538af8c49e0f62fc3fcb71391f43
Kacper Falecki <kfalecki17@gmail.com>
2023-02-11 13:49:27 +0100

    Add path '/' to RootLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import './App.css';
+import RootLoyout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {}
+  {
+    path: '/',
+    element: <RootLoyout />,
+    children: [
+
+    ],
+  },
 ])
 
 function App() {

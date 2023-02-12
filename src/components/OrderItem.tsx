88dd87057cec4d25b4834ce3deff6f987a944622
Kacper Falecki <kfalecki17@gmail.com>
2023-02-12 22:10:00 +0100

    Edit OrderDetails display

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -2,6 +2,9 @@ import classes from "../style/OrderItem.module.css";
 
 import { Button, createTheme, ThemeProvider } from "@mui/material";
 
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
 import { Link } from "react-router-dom";
 
 const theme = createTheme({
@@ -17,34 +20,35 @@ interface Order {
 }
 
 function OrderItem({ order }: Order) {
-  function startDeleteHandler() {
+  function DeleteHandler() {
     // ...
   }
 
   return (
     <ThemeProvider theme={theme}>
-      <article className={classes.event}>
-        <div className={classes.content}>
-          <h2>Address: {order.address}</h2>
-          <h3>Roof paint: {order.roofPaint}</h3>
-          <h3>Roof size: {order.roofSize} square meters</h3>
-          <h3>Roof angle: {order.roofAngle} degrees</h3>
-          <h3>Description: {order.description}</h3>
-        </div>
-        <menu className={classes.actions}>
-          <Button variant="contained" color="primary">
-            <Link to="edit">
-              Edit
-            </Link>
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-          >
-            Delete
-          </Button>
-        </menu>
-      </article>
+      <h1>Order Details</h1>
+      <Grid className={classes.order} item xs={12}>
+        <Paper
+          className={classes.paper}
+          sx={{ m: 2, p: 2, display: "flex", flexDirection: "column" }}
+        >
+          <div className={classes.content}>
+            <h2>Address: {order.address}</h2>
+            <h3>Roof paint: {order.roofPaint}</h3>
+            <h3>Roof size: {order.roofSize} square meters</h3>
+            <h3>Roof angle: {order.roofAngle} degrees</h3>
+            <h3>Description: {order.description}</h3>
+          </div>
+        </Paper>
+      </Grid>
+      <menu className={classes.actions}>
+        <Button variant="contained" color="primary">
+          <Link to="edit">Edit</Link>
+        </Button>
+        <Button onClick={DeleteHandler} variant="contained" color="primary">
+          Delete
+        </Button>
+      </menu>
     </ThemeProvider>
   );
 }

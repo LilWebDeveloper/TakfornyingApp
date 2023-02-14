bbd998ad735f9c1c911f41ddfb8e694ada8c4e3b
Kacper Falecki <kfalecki17@gmail.com>
2023-02-14 12:28:30 +0100

    Add list component with bugs

--- src/components/employees/EmployeesList.tsx
+++ src/components/employees/EmployeesList.tsx
@@ -1,30 +1,7 @@
-import { Link } from "react-router-dom";
-
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-
-import classes from "../../style/List.module.css";
-
+import List from "../items/List";
 
 const EmployeesList = ({ employees }: any) => {
-  return (
-    <div className={classes.orders}>
-      <h1>All Employees</h1>
-        {employees.map((employee: any) => (
-          <Grid key={employee.id} item xs={12} >
-            <Paper className={classes.paper} sx={{m: 2, p: 2, display: "flex", flexDirection: "column" }}>
-                <Link to={`/employees/${employee.id}`}>
-                  <div className={classes.content}>
-                    <h2>{employee.firstName} {employee.secondName}</h2>
-                    <h3>{employee.jobPosition}</h3>
-                  </div>
-                </Link>
-            </Paper>
-          </Grid>
-        ))}
-     
-    </div>
-  );
+  return <List title='All employees' dataset={employees}/>
 };
 
 export default EmployeesList;
--- src/components/orders/OrdersList.tsx
+++ src/components/orders/OrdersList.tsx
@@ -1,32 +1,7 @@
-import { Link } from "react-router-dom";
+import List from "../items/List";
 
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-
-import classes from "../../style/List.module.css";
-
-interface Orders {
-  orders: any;
-}
-
-const OrdersList = ({ orders }: Orders) => {
-  return (
-    <div className={classes.orders}>
-      <h1>All Orders</h1>
-        {orders.map((order: any) => (
-          <Grid key={order.id} item xs={12} >
-            <Paper className={classes.paper} sx={{m: 2, p: 2, display: "flex", flexDirection: "column" }}>
-                <Link to={`/orders/${order.id}`}>
-                  <div className={classes.content}>
-                    <h2>{order.address}</h2>
-                  </div>
-                </Link>
-            </Paper>
-          </Grid>
-        ))}
-     
-    </div>
-  );
+const OrdersList = ({ orders }: any) => {
+  return <List title='All orders' dataset={orders}/>
 };
 
 export default OrdersList;

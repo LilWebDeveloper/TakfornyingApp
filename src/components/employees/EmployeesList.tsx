6750508f6f22b7c493af721eb530e120b841c045
Kacper Falecki <kfalecki17@gmail.com>
2023-02-16 13:36:56 +0100

    Delete usless List.tsx

--- src/components/employees/EmployeesList.tsx
+++ src/components/employees/EmployeesList.tsx
@@ -1,7 +1,33 @@
-import List from "../items/List";
+import { Link } from "react-router-dom";
+
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+import classes from "../../style/List.module.css";
 
 const EmployeesList = ({ employees }: any) => {
-  return <List title='All employees' dataset={employees}/>
+  return (
+    <div className={classes.orders}>
+      <h1>All Employees</h1>
+      {employees.map((data: any) => (
+        <Grid key={data.id} item xs={12}>
+          <Paper
+            className={classes.paper}
+            sx={{ m: 2, p: 2, display: "flex", flexDirection: "column" }}
+          >
+            <Link to={`/employees/${data.id}`}>
+              <div className={classes.content}>
+                <h2>
+                  {data.firstName} {data.secondName}
+                </h2>
+                <h3>{data.jobPosition}</h3>
+              </div>
+            </Link>
+          </Paper>
+        </Grid>
+      ))}
+    </div>
+  );
 };
 
 export default EmployeesList;
--- src/components/orders/OrdersList.tsx
+++ src/components/orders/OrdersList.tsx
@@ -1,7 +1,32 @@
-import List from "../items/List";
+import { Link } from "react-router-dom";
+
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+import classes from "../../style/List.module.css";
 
 const OrdersList = ({ orders }: any) => {
-  return <List title='All orders' dataset={orders}/>
+  return (
+    <div className={classes.orders}>
+      <h1>All orders</h1>
+      {orders.map((data: any) => (
+        <Grid key={data.id} item xs={12}>
+          <Paper
+            className={classes.paper}
+            sx={{ m: 2, p: 2, display: "flex", flexDirection: "column" }}
+          >
+          
+              <Link to={`/orders/${data.id}`}>
+                <div className={classes.content}>
+                  <h2>{data.address}</h2>
+                </div>
+              </Link>
+            
+          </Paper>
+        </Grid>
+      ))}
+    </div>
+  );
 };
 
 export default OrdersList;

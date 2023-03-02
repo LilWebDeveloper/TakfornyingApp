95f9166cfe76c53f6b14aa380b60c7cbfc07229b
Kacper Falecki <kfalecki17@gmail.com>
2023-03-02 14:50:59 +0100

    Add backend to order, employee with delete function

--- src/services/actions/DeleteEmployeeAction.tsx
+++ src/services/actions/DeleteEmployeeAction.tsx
@@ -1,22 +1,14 @@
 import { redirect, json } from "react-router-dom";
 
 export default async function DeleteEmployeeAction({ params, request }: any) {
-    const employeeId = params.employeeId;
-    const response = await fetch(
-      "https://takfornyingmenagmentapp-default-rtdb.europe-west1.firebasedatabase.app/employees/" +
-        employeeId +
-        ".json", {
-          method: request.method,
-        }
-    );
-  
-    if (!response.ok) {
-      throw json(
-        { message: "Could not delete employee." },
-        { status: 500 }
-      );
-    }
-  
-    return redirect('/dashboard/employees')
+  const employeeId = params.employeeId;
+  const response = await fetch("http://localhost:5050/employees/" + employeeId, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete employee." }, { status: 500 });
   }
 
+  return redirect("/dashboard/employees");
+}
--- src/services/actions/DeleteOrderAction.tsx
+++ src/services/actions/DeleteOrderAction.tsx
@@ -1,21 +1,14 @@
 import { redirect, json } from "react-router-dom";
 
 export default async function DeleteOrderAction({ params, request }: any) {
-    const orderId = params.orderId;
-    const response = await fetch(
-      "https://takfornyingmenagmentapp-default-rtdb.europe-west1.firebasedatabase.app/orders/" +
-        orderId +
-        ".json", {
-          method: request.method,
-        }
-    );
-  
-    if (!response.ok) {
-      throw json(
-        { message: "Could not delete order." },
-        { status: 500 }
-      );
-    }
-  
-    return redirect('/dashboard/orders')
+  const orderId = params.orderId;
+  const response = await fetch("http://localhost:5050/orders/" + orderId, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete order." }, { status: 500 });
   }
+
+  return redirect("/dashboard/orders");
+}

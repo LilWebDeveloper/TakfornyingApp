46e5740346341e33f74007de3c0fc7bc12426752
Kacper Falecki <kfalecki17@gmail.com>
2023-03-03 10:20:48 +0100

    Remove /edit and /new from database url

--- src/services/actions/ManipulateEmployeeAction.tsx
+++ src/services/actions/ManipulateEmployeeAction.tsx
@@ -1,39 +1,40 @@
 import { redirect } from "react-router-dom";
 
-export default async function ManipulateEmployeeAction({ request, params }: any) {
-    const method = request.method;
-  
-    const data = await request.formData();
-  
-    const employeeData = {
-      firstName: data.get("firstName"),
-      secondName: data.get("secondName"),
-      jobPosition: data.get("jobPosition"),
-      dNumber: data.get("dNumber"),
-      login: data.get("userLogin"),
-      password: data.get("password"),
-    };
-  
-    let url = 'http://localhost:5050/employees/new'
-  
-    if(method === 'PATCH'){
-      const employeeId = params.employeeId
-      url = 'http://localhost:5050/employees/' + employeeId + '/edit'
-    }
-  
-    const response = await fetch(url ,
-      {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(employeeData),
-      }
-    );
-  
-    if (!response.ok) {
-      console.log(employeeData);
-    }
-  
-    return redirect("/dashboard/employees");
+export default async function ManipulateEmployeeAction({
+  request,
+  params,
+}: any) {
+  const method = request.method;
+
+  const data = await request.formData();
+
+  const employeeData = {
+    firstName: data.get("firstName"),
+    secondName: data.get("secondName"),
+    jobPosition: data.get("jobPosition"),
+    dNumber: data.get("dNumber"),
+    login: data.get("userLogin"),
+    password: data.get("password"),
+  };
+
+  let url = "http://localhost:5050/employees";
+
+  if (method === "PATCH") {
+    const employeeId = params.employeeId;
+    url = "http://localhost:5050/employees/" + employeeId;
   }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employeeData),
+  });
+
+  if (!response.ok) {
+    console.log(employeeData);
+  }
+
+  return redirect("/dashboard/employees");
+}
--- src/services/actions/ManipulateOrderAction.tsx
+++ src/services/actions/ManipulateOrderAction.tsx
@@ -1,38 +1,37 @@
 import { redirect } from "react-router-dom";
 
 export default async function ManipulateOrderAction({ request, params }: any) {
-    const method = request.method;
-  
-    const data = await request.formData();
-  
-    const orderData = {
-      address: data.get("address"),
-      roofPaint: data.get("roofPaint"),
-      roofSize: data.get("roofSize"),
-      roofAngle: data.get("roofAngle"),
-      description: data.get("description"),
-      worker: data.get('worker')
-    };
-  
-    let url =
-      "http://localhost:5050/orders/new";
-  
-      if(method === 'PATCH'){
-        const orderId = params.orderId;
-        url = "http://localhost:5050/orders/" + orderId + '/edit'
-      }
-  
-    const response = await fetch(url, {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(orderData),
-    });
-  
-    if (!response.ok) {
-      console.log(orderData);
-    }
-  
-    return redirect("/dashboard/orders");
+  const method = request.method;
+
+  const data = await request.formData();
+
+  const orderData = {
+    address: data.get("address"),
+    roofPaint: data.get("roofPaint"),
+    roofSize: data.get("roofSize"),
+    roofAngle: data.get("roofAngle"),
+    description: data.get("description"),
+    worker: data.get("worker"),
+  };
+
+  let url = "http://localhost:5050/orders";
+
+  if (method === "PATCH") {
+    const orderId = params.orderId;
+    url = "http://localhost:5050/orders/" + orderId;
   }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(orderData),
+  });
+
+  if (!response.ok) {
+    console.log(orderData);
+  }
+
+  return redirect("/dashboard/orders");
+}
